a43af5683e5aec855f2df972cf81645aee08136a
pranavadurai <pranavadurai@gmail.com>
2016-08-12 08:21:23 +0530

    recent update

--- signin.php
+++ signin.php
@@ -2,25 +2,31 @@
 session_start();
 $short = $_POST['short'];
 $password = $_POST['password'];
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn=new mysqli("localhost","root","12345","csc");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM Profile where shortid ='".$short."'";
-$result = $conn->query($sql);
-    while($row = $result->fetch_assoc())
+if(($conn->query($sql))==true)
+{
+    while($row = $result->fetch_array())
     {
-        if($row["password"] == $password)
+        if($row['password'] == $password)
         {
-          $_SESSION["name"] = $row["name"];
-          $_SESSION["short"] = $row["shortid"];
+          $_SESSION['user_id'] = $row['id'];
+          $_SESSION['name'] = $row['name'];
+          $_SESSION['short'] = $row['short_id'];
           header('Location:index.php');
         }
         else {
-          echo "Something Wrong";
+          echo "Password Wrong";
         }
     }
+  }
+  else {
+    echo "table select error";
+  }
 
 $conn->close();
  ?>
